6be5e0cbd157db28b9cd189678b14d5b994e21f3
Willis Pinaud <willispinaud@gmail.com>
2015-10-21 17:59:52 +0200

    add anim and gravite

--- JeuDeDames/JeuDeDames/monSuperCallback.hpp
+++ JeuDeDames/JeuDeDames/monSuperCallback.hpp
@@ -23,18 +23,24 @@ class monSuperCallback : public osg::NodeCallback
 public:
     void operator()(osg::Node* noeud, osg::NodeVisitor* visiteur){
         //x: pat->getPosition()*Resultant.x;
+        double r = rand()%10;
         osg::ref_ptr<osg::MatrixTransform> mt = dynamic_cast<osg::MatrixTransform*>(noeud->getParent(0));
-        osg::Vec4d restest = osg::Vec4d(0,0,0,0);
+        osg::Vec4d restest = osg::Vec4d(0,0,-r/10,0);
         
-        osg::Vec4d resultante = mt->getMatrix()*restest;
+        osg::Vec4d normale = mt->getMatrix()*restest;
+        osg::Vec4d resultante = normale - restest;
         osg::ref_ptr<osg::PositionAttitudeTransform> pat = dynamic_cast<osg::PositionAttitudeTransform*>(noeud);
+        
         osg::Vec3d posPrev = osg::Vec3d(0,0,0);
         
         if (!pat.valid()) {
             return;
         }
+
         posPrev = pat->getPosition();
-        osg::Vec3d pos = osg::Vec3d(resultante.x(),resultante.y(),resultante.z()) + posPrev;
+        
+        osg::Vec3d pos = osg::Vec3d(resultante.x(),resultante.y(),0) + posPrev;
+        
         pat->setPosition(pos);
         
         
